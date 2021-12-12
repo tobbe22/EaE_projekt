a8dd5dbee79a55ee42852a9ed455cb780e616d1b
ecker <eckernikolai@web.de>
2021-12-12 16:24:43 +0100

    geändert für Testzweck

--- app/src/main/java/com/example/eae_projekt/MainActivity.java
+++ app/src/main/java/com/example/eae_projekt/MainActivity.java
@@ -1,14 +1,176 @@
 package com.example.eae_projekt;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+DBmain dBmain;
+SQLiteDatabase sqLiteDatabase;
+EditText name;
+Button submit,display,edit;
+private ImageView avatar;
+
+public static final int CAMERA_REQUEST=100;
+public static final int STORAGE_REQUEST=101;
+
+String cameraPermission[];
+String storagePermission[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dBmain = new DBmain(this);
+        findid();
+        insertData();
+
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                int avatar=0;
+                if(avatar == 0){
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }else{
+                        pickFromGallery();
+                    }
+                }else if (avatar == 1){
+                    if(!checkStoragePermission()){
+                        requestStoragePermission();
+                    }else{
+                        pickFromGallery();
+                    }
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestStoragePermission() {
+        requestPermissions(storagePermission, STORAGE_REQUEST);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result=ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void pickFromGallery() {CropImage.activity().start(this);}
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestCameraPermission() {
+        requestPermissions(cameraPermission,CAMERA_REQUEST);
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==(PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+    private void insertData() {
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues cv=new ContentValues();
+                cv.put("name", name.getText().toString());
+                cv.put("avatar", imageViewToBy(avatar));
+
+                sqLiteDatabase = dBmain.getWritableDatabase();
+                Long rec=sqLiteDatabase.insert("course",null, cv);
+                if(rec != null){
+                    Toast.makeText(MainActivity.this,"data inserted", Toast.LENGTH_SHORT).show();
+                    name.setText("");
+                    avatar.setImageResource(R.mipmap.ic_launcher);
+                }else{
+                    Toast.makeText(MainActivity.this, "something wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public static byte[] imageViewToBy(ImageView avatar) {
+        Bitmap bitmap=((BitmapDrawable)avatar.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+        byte[]bytes= stream.toByteArray();
+        return bytes;
+    }
+
+    private void findid() {
+        name=(EditText)findViewById(R.id.edtname);
+        avatar=(ImageView)findViewById(R.id.edtimage);
+        submit=(Button)findViewById(R.id.btn_submit);
+        display=(Button)findViewById(R.id.btn_display);
+        edit=(Button)findViewById(R.id.btn_edit);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case CAMERA_REQUEST:{
+                if(grantResults.length > 0){
+                    boolean camera_accepted=grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                    boolean storage_accepted=grantResults[1]==PackageManager.PERMISSION_GRANTED;
+                    if(camera_accepted && storage_accepted){
+                       pickFromGallery();
+                    }else{
+                        Toast.makeText(this,"enable camera and storage permission",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST:{
+                if(grantResults.length>0){
+                    boolean storage_accepted=grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                    if(storage_accepted){
+                        pickFromGallery();
+                    }else{
+                        Toast.makeText(this,"please enable storage permission", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result= CropImage.getActivityResult(data);
+            if(resultCode==RESULT_OK){
+                Uri resultUri=result.getUri();
+                Picasso.with(this).load(resultUri).into(avatar);
+            }
+        }
     }
 }
